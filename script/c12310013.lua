6e0521a0a34d0c64b3f82fae1047ac3611b815bc
Astral-1 <61868752+Astral-1@users.noreply.github.com>
2022-09-03 23:32:46 +0200

    Add files via upload

--- script/c12310013.lua
+++ script/c12310013.lua
@@ -1,5 +1,5 @@
---超重神鬼シュテンドウ－G
---Superheavy Samurai Ogre Shutendoji
+--Superheavy Samurai Swordsmith Musashi
+--Scripted by UNMEI for Itsu RP
 local s,id=GetID()
 function s.initial_effect(c)
 	--synchro summon
